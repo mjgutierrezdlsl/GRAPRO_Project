afcf53e4572d25977554fe29eb0b5fa8abfd2990
Jolo Gutierrez <mariano.jose.gutierrez.iii@dlsl.edu.ph>
2025-05-06 18:57:42 +0800

    rewrote camera script to better follow CodingWithSphere's example

--- Camera.cs
+++ Camera.cs
@@ -1,92 +1,77 @@
 
+using OpenTK.Graphics.ES11;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 
 public class Camera
 {
-    private Vector3 Position;
-    private Vector3 Front = -Vector3.UnitZ;
-    private Vector3 Up = Vector3.UnitY;
-    private Vector3 Right = Vector3.UnitX;
-    private float Speed = 1.5f;
-    private float AspectRatio;
-    private float Pitch;
-    private float Yaw = -MathHelper.PiOver2;
-    private bool _firstMove = true;
-    private Vector2 _lastPos;
-    private float Sensitivity = 0.2f;
-    public Camera(Vector3 Position, float AspectRatio)
+    private float _speed;
+    private float _sensitivity;
+
+    private Vector3 _position;
+    private float _aspectRatio;
+    private float _fieldOfView;
+    private float _depthNear;
+    private float _depthFar;
+
+    private Vector3 _up = Vector3.UnitY;
+    private Vector3 _front = -Vector3.UnitZ;
+    private Vector3 _right = Vector3.UnitX;
+
+    public Camera(Vector3 position, float aspectRatio, float speed = 1.5f, float sensitivity = 0.2f, float fieldOfView = 45f, float depthNear = 0.1f, float depthFar = 100f)
     {
-        this.Position = Position;
-        this.AspectRatio = AspectRatio;
+        _position = position;
+        _aspectRatio = aspectRatio;
+        _speed = speed;
+        _sensitivity = sensitivity;
+        _fieldOfView = fieldOfView;
+        _depthNear = depthNear;
+        _depthFar = depthFar;
     }
+
     public Matrix4 GetViewMatrix()
     {
-        return Matrix4.LookAt(Position, Position + Front, Up);
+        return Matrix4.LookAt(_position, _position + _front, _up);
     }
+
     public Matrix4 GetProjectionMatrix()
     {
-        return Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45f), AspectRatio, 0.01f, 100f);
+        return Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(_fieldOfView), _aspectRatio, _depthNear, _depthFar);
     }
+
     public void ProcessInputs(KeyboardState input, MouseState mouse, FrameEventArgs e)
     {
-        float deltaTime = (float)e.Time;
+        var deltaTime = (float)e.Time;
 
         if (input.IsKeyDown(Keys.W))
         {
-            Position += Front * Speed * deltaTime;
+            _position += _front * _speed * deltaTime;
         }
         if (input.IsKeyDown(Keys.S))
         {
-            Position -= Front * Speed * deltaTime;
+            _position -= _front * _speed * deltaTime;
         }
         if (input.IsKeyDown(Keys.A))
         {
-            Position -= Vector3.Normalize(Vector3.Cross(Front, Up)) * Speed * deltaTime;
+            _position -= Vector3.Normalize(Vector3.Cross(_front, _up)) * _speed * deltaTime;
         }
         if (input.IsKeyDown(Keys.D))
         {
-            Position += Vector3.Normalize(Vector3.Cross(Front, Up)) * Speed * deltaTime;
+            _position += Vector3.Normalize(Vector3.Cross(_front, _up)) * _speed * deltaTime;
         }
         if (input.IsKeyDown(Keys.Space))
         {
-            Position.Y += Speed * deltaTime;
+            _position.Y += _speed * deltaTime;
         }
         if (input.IsKeyDown(Keys.LeftShift))
         {
-            Position.Y -= Speed * deltaTime;
-        }
-
-        if (_firstMove)
-        {
-            _lastPos = new(mouse.X, mouse.Y);
-            _firstMove = false;
-        }
-        else
-        {
-            var deltaX = mouse.X - _lastPos.X;
-            var deltaY = mouse.Y - _lastPos.Y;
-            _lastPos = new(mouse.X, mouse.Y);
-            Yaw += deltaX * Sensitivity * deltaTime;
-            Pitch -= deltaY * Sensitivity * deltaTime;
-            UpdateVectors();
+            _position.Y -= _speed * deltaTime;
         }
     }
+
     public void SetAspectRatio(float width, float height)
     {
-        AspectRatio = width / height;
-    }
-    private void UpdateVectors()
-    {
-        Pitch = MathHelper.Clamp(Pitch, -89f, 89f);
-        Front.X = MathF.Cos(Pitch) * MathF.Cos(Yaw);
-        Front.Y = MathF.Sin(Pitch);
-        Front.Z = MathF.Cos(Pitch) * MathF.Sin(Yaw);
-
-        Front = Vector3.Normalize(Front);
-
-        Right = Vector3.Normalize(Vector3.Cross(Front, Vector3.UnitY));
-        Up = Vector3.Normalize(Vector3.Cross(Right, Front));
+        _aspectRatio = width / height;
     }
 }
